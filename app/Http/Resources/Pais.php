7852d15e37c945a52de9a2e60b5884f32e92943c
Julian Casaburi <juliancasaburi@gmail.com>
2021-10-28 02:31:00 -0300

    ✨ Se agrega países hacia los que no se exporta

--- app/Http/Resources/Pais.php
+++ app/Http/Resources/Pais.php
@@ -15,8 +15,6 @@ class Pais extends JsonResource
     public function toArray($request)
     {
         return [
-            'id' => $this->id,
-            'code' => $this->code,
             'name' => $this->name,
         ];
     }
